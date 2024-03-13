180e161f6c8f1a438c569e601a66772bc21ae886
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-03-13 15:03:34 +0100

    Struttura esercizio

---
+++ src/entities/Order.java
@@ -0,0 +1,50 @@
+package entities;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Order {
+
+    private long id;
+    private String status;
+    private LocalDate orderDate;
+
+    private LocalDate deliveryDate;
+
+    private List<Product> products;
+    private Customer customer;
+
+    public Order(long id, String status, LocalDate orderDate, LocalDate deliveryDate, List<Product> products, Customer customer) {
+        this.id = id;
+        this.status = status;
+        this.orderDate = orderDate;
+        this.deliveryDate = deliveryDate;
+        this.products = products;
+        this.customer = customer;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public LocalDate getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+}
+
